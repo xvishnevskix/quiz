24951d4e94dd57dc91341ed3a71498e80d305a03
xvishnevskix <45593711+xvishnevskix@users.noreply.github.com>
2022-12-05 10:29:56 +0400

    progressbar, correct answers

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './index.scss';
+import React from "react";
 
 const questions = [
   {
@@ -22,37 +23,53 @@ const questions = [
   },
 ];
 
-function Result() {
+function Result({correctValue}) {
   return (
     <div className="result">
       <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-      <h2>Вы отгадали 3 ответа из 10</h2>
+      <h2>Вы отгадали {correctValue} ответа из {questions.length}</h2>
       <button>Попробовать снова</button>
     </div>
   );
 }
 
-function Game() {
+function Game({question, onClickVariant, step}) {
+    const percentage = Math.round((step / questions.length) * 100)
+    console.log(percentage)
   return (
     <>
       <div className="progress">
-        <div style={{ width: '50%' }} className="progress__inner"></div>
+        <div style={{ width: `${percentage}%` }} className="progress__inner"></div>
       </div>
-      <h1>Что такое useState?</h1>
+      <h1>{question.title}</h1>
       <ul>
-        <li>Это функция для хранения данных компонента</li>
-        <li>Это глобальный стейт</li>
-        <li>Это когда на ты никому не нужен</li>
+          {
+              question.variants.map((text, index) => (
+                 <li onClick={() => onClickVariant(index)} key={index}>{text}</li>
+              ))
+          }
       </ul>
     </>
   );
 }
 
 function App() {
+
+    const [step, setStep] = React.useState(0)
+    const question = questions[step]
+    const [correct, setCorrect] = React.useState(0)
+    const onClickVariant = (index) => {
+        setStep(step+1)
+        if (questions[index].correct === question.correct) {
+            setCorrect(correct + 1)
+        }
+    }
   return (
     <div className="App">
-      <Game />
-      {/* <Result /> */}
+
+      { step !== questions.length ? (
+          <Game step={step} question={question} onClickVariant={onClickVariant}/>
+      ) : (<Result correctValue={correct}/>)}
     </div>
   );
 }
